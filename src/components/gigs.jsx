0b92cf9784f38b67692a5e27ae955e760d743bf2
ninyhorlah <58943705+ninyhorlah@users.noreply.github.com>
2021-08-17 10:20:02 +0100

    top nav

--- src/components/gigs.jsx
+++ src/components/gigs.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import SideBar from './nav/sideBar'
+import TopNav from './nav/topNav';
 
 const Gigs = () => {
     return ( 
         <div>
+            <TopNav />
             <SideBar/>
         </div>
      );
--- src/components/nav/topNav.jsx
+++ src/components/nav/topNav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import notification from '../../asset/notification.png'
+import email from '../../asset/email.png'
+import settings from '../../asset/settings.png'
+import profile from '../../asset/profile.png'
 
 const TopNav = () => {
     return ( 
-        <div>
-            
+        <div className='topNavContainer'>
+            <div className='searchBar'>
+                <input type="text" placeholder='Search'/>
+            </div>
+            <div className='navItem'>
+                <img src={notification} alt="" title='notification'/>
+                <img src={email} alt="" title='email'/>
+                <img src={settings} alt="" title='settings'/>
+                <img src={profile} alt="" title='profile'/>
+            </div>
         </div>
      );
 }
